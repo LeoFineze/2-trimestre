ade073658c0309906bb6fa8490a63d1205b73b46
LeoFineze <128715920+LeoFineze@users.noreply.github.com>
2024-05-28 08:49:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativa");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado:"Quem tem copa do mundo?",
+    alternativas:[
+      "Lionel Messi",
+      "Cristiano Ronaldo",
+    ]
+  },
+  {
+    enunciado:"Quantas bolas de ouro messi possui?",
+    alternativas:[
+      " 8 ",
+      " 5 ",
+    ]
+    
+  }, 
+  enunciado:"Quem criou a apple?",
+    alternativas:[
+      "alternativa 1",
+      "alternativa 2",
+    ]
+
+  }, 
+  enunciado:"Pergunta 4",
+    alternativas:[
+      "alternativa 1",
+      "alternativa 2",
+    ]
+
+  }, 
+  enunciado:"Pergunta 5",
+    alternativas:[
+      "alternativa 1",
+      "alternativa 2",
+    ]
+  }, 
+];
